3a65374c6e9074b911372f14c1bb19914bbb5178
krishnabhusare <krishnabhusare1996@gmail.com>
2024-09-01 11:04:04 +0530

    cartadded

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,4 +1,5 @@
 const Product = require('../models/product');
+const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
@@ -10,13 +11,17 @@ exports.getProducts = (req, res, next) => {
   });
 };
 
-exports.getProduct=(req,res,next)=>{
+exports.getProduct = (req,res,next)=>{
   const prodId=req.params.productId;
-  Product.findById(prodId,product=>{
-    res.render('shop/product-detail',{product:product,pageTitle:product.title,path:'/products'})
+  Product.findById(prodId, product=>{
+    res.render('shop/product-detail',{
+      product: product,
+      pageTitle: product.title,
+      path: '/products'
+    });
   });
   
-}
+};
 
 exports.getIndex = (req, res, next) => {
   Product.fetchAll(products => {
@@ -33,6 +38,14 @@ exports.getCart = (req, res, next) => {
     path: '/cart',
     pageTitle: 'Your Cart'
   });
+};
+
+exports.postCart = (req,res,next)=>{
+  const prodId= req.body.productId;
+ Product.findById(prodId,(product)=>{
+      Cart.addProduct(prodId, product.price);
+ })
+  res.redirect('/cart');
 };
 
 exports.getOrders = (req, res, next) => {
--- models/product.js
+++ models/product.js
@@ -40,9 +40,10 @@ module.exports = class Product {
   }
 
   static findById(id,cb){
-     getProductsFromFile(products=>{
+    getProductsFromFile(products=>{
       const product=products.find(p=>p.id===id);
       cb(product);
-     })
-  }
+    });
+
+  };
 };
