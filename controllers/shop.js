90ebc857b689018d17ee58cb6e611ea1da8658a4
krishnabhusare <krishnabhusare1996@gmail.com>
2024-09-14 11:35:28 +0530

    deletefunctanlitysequelize

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,47 +1,51 @@
 const Product = require('../models/product');
 const Cart = require('../models/cart');
+const { where } = require('sequelize');
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll()
-   .then(([rows, fieldData])=>{
+  Product.findAll()
+  .then(products =>{
     res.render('shop/product-list', {
-      prods: rows,
+      prods: products,
       pageTitle: 'All Products',
       path: '/products'
     });
-  }) 
+  })
   .catch(err=>{
     console.log(err);
   });
-  
+ 
 };
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
-  Product.findById(prodId).then(([product])=>{
+  Product.findByPk(prodId)
+  .then(product=>{
     res.render('shop/product-detail', {
-      product: product[0],
+      product: product,
       pageTitle: product.title,
       path: '/products'
     });
-  }).catch(err=>{
+  })
+  .catch(err=>{
     console.log(err);
   });
   
 };
 
 exports.getIndex = (req, res, next) => {
-  Product.fetchAll()
-  .then(([rows, fieldData])=>{
+  Product.findAll()
+  .then(products =>{
     res.render('shop/index',{
-      prods: rows,
+      prods: products,
       pageTitle: 'shop',
       path: '/'
     });
-  }) 
+  })
   .catch(err=>{
     console.log(err);
   });
+ 
 };
 
 exports.getCart = (req, res, next) => {
